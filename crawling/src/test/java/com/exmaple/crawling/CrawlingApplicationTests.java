a894f3bd7474f32890d9ae8ebf97fb15f7ae429a
한동근 <ehdrms001030@naver.com>
2025-09-04 23:02:33 +0900

    project init

---
+++ crawling/src/test/java/com/exmaple/crawling/CrawlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exmaple.crawling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
